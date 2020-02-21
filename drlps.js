a9fa024e5adef98497201befe6a2bb5dd7696805
Matt Ryan <mattryanjersey@gmail.com>
2020-02-21 22:13:48 +0000

    added js to control the menu toggle and fixed bug where menu may not show depending on toggled option when sized > 768 pixels

--- drlps.js
+++ drlps.js
@@ -1,4 +1,15 @@
-$("#menu").toggle();
+function show_hide_menu(screen_width) {
+  if (screen_width <= 768) {
+    $("#menu").hide();
+  } else {
+    $("#menu").show();
+  }
+}
+
+$(window).resize(function () {
+  show_hide_menu($(window).width());
+});
+
 
 $("#menu-btn").click(function () {
   $("#menu").toggle();
